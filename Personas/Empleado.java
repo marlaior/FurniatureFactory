cb4d402351ab1ebb2529d56d216ce8c6215cf38a
starwatcher <marlaior@hotmail.com>
2020-07-28 21:10:10 +0200

    Definiendo atributos de personas

--- Personas/Empleado.java
+++ Personas/Empleado.java
@@ -1,6 +1,6 @@
 package Personas;
 
-
+import java.util.Date;
 /**
  * Clase que describe objetos de tipo Empleado
  * Esta clase hereda de Persona
@@ -8,5 +8,10 @@ package Personas;
  * @author Marcos Laíño Ordóñez
  */
 public class Empleado extends Persona{
+    protected static int id_empleado;
+    protected String puesto;
+    protected Date fechaAlta;
+    protected Date fechaBaja;
+    protected double salarioBase;
 
 }
--- Personas/Persona.java
+++ Personas/Persona.java
@@ -7,4 +7,22 @@ package Personas;
  * @author Marcos Laíño Ordóñez
  */
 public class Persona{
+    protected String usuario;
+    protected String contraseña;
+    protected String nombre;
+    protected String apellidos;
+    protected String telefono;
+    
+    /**
+     * Contructor de la clase Persona
+     */
+    public Persona(String usuario, String contraseña, String nombre, String apellidos, String telefono){
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        
+    }
+
 }
