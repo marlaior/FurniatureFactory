93ffc8cd4ccd6e93488c94a76d53512d396342fd
starwatcher <marlaior@hotmail.com>
2020-08-31 16:53:31 +0200

    gestion de estados de pedidos

---
+++ Fabrica/__SHELL57.java
@@ -0,0 +1,8 @@
+package fabrica;
+public class __SHELL57 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+fabrica.Fabrica.main(__bluej_param0);
+
+}}
